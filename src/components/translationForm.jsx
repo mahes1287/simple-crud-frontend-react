60ca9410d0d6f38892122c9b231b5efc044e8fc5
mahes1287 <mahes1287@gmail.com>
2023-01-25 13:16:09 +0100

    added tailwind

---
+++ src/components/translationForm.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function translationForm() {
+  return <div>translationForm</div>;
+}
---
+++ src/components/translationList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function translationList() {
+  return <div>translationList</div>;
+}
