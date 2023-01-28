ca21ba2fe06ad0899d8dbc66e86e2316f207b52b
mahes1287 <mahes1287@gmail.com>
2023-01-28 23:22:43 +0100

    Delete translation

--- src/components/DeleteDialog.jsx
+++ src/components/DeleteDialog.jsx
@@ -1,7 +1,82 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import AuthContext from "../contexts/AuthContext";
+import axios, * as others from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
-export default function DeleteDialog() {
+const DeleteDialog = ({ id }) => {
+  const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
+  auth.onIdTokenChanged(async (user) => {
+    const token = await user?.getIdToken();
+    localStorage.setItem("token", token);
+  });
+
+  async function deleteTranslation() {
+    let config = {
+      method: "delete",
+      url: `http://127.0.0.1:8000/api/translations/${id}/delete`,
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    };
+    try {
+      const response = await axios(config);
+      setShowModal(false)
+      navigate("/translations");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div>DeleteDialog</div>
-  )
-}
+    <>
+      <button
+        className="m-2 font-semibold bg-red-100 hover:bg-red-400 px-4 rounded-lg py-2"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Delete
+      </button>
+      {showModal ? (
+        <>
+          <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 rounded-t ">
+                  <h3 className="text-xl ">Are you sure to delete this? </h3>
+                  <button
+                    className="bg-transparent border-0 text-black float-right"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="text-black opacity-7 h-6 w-6 text-xl block bg-gray-400 py-0 rounded-full">
+                      x
+                    </span>
+                  </button>
+                </div>
+                <div className="flex items-center justify-end p-6 rounded-b">
+                  <button
+                    className="text-red-300  hover:text-red-700 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    className="text-white bg-red-400 hover:bg-red-700 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => deleteTranslation()}
+                  >
+                    Yes, Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default DeleteDialog;
