32e3c6af23dc5e33612702d86b9908a8ac635491
mahes1287 <mahes1287@gmail.com>
2023-01-28 13:04:46 +0100

    init

---
+++ src/components/TranslationCard.jsx
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
+import AuthContext from "../contexts/AuthContext";
+
+export default function TranslationCard() {
+  const { user, loading, error, auth } = useContext(AuthContext);
+  auth.onIdTokenChanged(async (user) => {
+    const token = await user?.getIdToken();
+    localStorage.setItem("token", token);
+  });
+  const {
+    state: { input, output, id, fromUser },
+  } = useLocation();
+  return (
+    <div
+      key={id}
+      className="flex flex-col space-y-2 max-w-7xl ml-5 mx-auto bg-orange-100 m-5 rounded-lg shadow-md hover:bg-orange-200"
+    >
+      <div className="text-xl ml-3 py-3">
+        <span className="italic ">input: </span> {input}
+      </div>
+      <div className="text-xl ml-3 py-3">
+        <span className="italic ">output: </span> {output}
+      </div>
+      <div className="text-xl ml-3 py-3">
+        <span className="italic ">created by: </span> {fromUser}
+      </div>
+      <div className="flex sm:flex-col md:flex-row justify-center items-center">
+        <Link
+          to={`/translations/${id}/update`}
+          state={{ input: input, output: output, id: id }}
+          className="m-2 font-semibold bg-blue-100 hover:bg-blue-400 px-4 rounded-lg py-2"
+        >
+          edit
+        </Link>
+        <button className="m-2 font-semibold bg-red-100 hover:bg-red-400 px-4 rounded-lg py-2">
+          delete
+        </button>
+      </div>
+    </div>
+  );
+}
