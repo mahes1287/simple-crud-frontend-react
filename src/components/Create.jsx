b61ecd85d9d9e07c7d8fa26aa854e8271ace18ba
mahes1287 <mahes1287@gmail.com>
2023-01-28 13:05:35 +0100

    update

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../contexts/AuthContext";
 import TranslationForm from "./TranslationForm";
 
 export default function Create() {
+  const { user } = useContext(AuthContext);
   return (
     <div>
       <div>Create new translation</div>
-      <TranslationForm />
+      <TranslationForm input="" output="" />
     </div>
   );
 }
