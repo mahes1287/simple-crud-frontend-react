8ea42a314c1afe8118553fe058625dcace82b396
mahes1287 <mahes1287@gmail.com>
2023-01-29 01:05:00 +0100

    user specific welcome message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Home() {
-  return (
-    <div>You are at sweet home!!!!!</div>
-  )
+  const { user } = useAuth();
+  if (user) {
+    return <h1>Hello {user?.displayName} welcome to our app</h1>;
+  }
+
+  return <div>You are at sweet home!!!!!</div>;
 }
