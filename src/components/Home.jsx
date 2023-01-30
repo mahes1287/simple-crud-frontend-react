44526fae0fd60754afdbabfc11a864bfba78ebdb
mahes1287 <mahes1287@gmail.com>
2023-01-30 21:37:39 +0100

    getting displayName from localStorage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,13 @@ import { useAuth } from "../contexts/AuthContext";
 export default function Home() {
   const { user } = useAuth();
   if (user) {
-    return <h1>Hello {user?.displayName} welcome to our app</h1>;
+    return (
+      <>
+        {" "}
+        <h1>Hello {localStorage.getItem("displayName")} welcome to our app</h1>
+        <div>You are at sweet home!!!!!</div>
+      </>
+    );
   }
 
   return <div>You are at sweet home!!!!!</div>;
