42ca75a6433b541a5b28ec48707b1af72f5a1a03
mahes1287 <mahes1287@gmail.com>
2023-01-30 21:48:18 +0100

    conditional rendering for edit and delete button

--- src/components/TranslationView.jsx
+++ src/components/TranslationView.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import DeleteDialog from "./DeleteDialog";
 import axios from "axios";
+
 export default function TranslationView() {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
@@ -46,16 +48,34 @@ export default function TranslationView() {
           <div className="text-xl ml-3 py-3">
             <span className="italic ">created by: </span> {data.fromUser}
           </div>
-          <div className="flex sm:flex-col md:flex-row justify-center items-center">
-            <Link
-              to={`/translations/${id}/update/`}
-              state={{ input: data.input, output: data.output, id: id }}
-              className="m-2 font-semibold bg-blue-100 hover:bg-blue-400 px-4 rounded-lg py-2"
+
+          {localStorage.getItem("displayName") ? (
+            <>
+              <div className="flex sm:flex-col md:flex-row justify-center items-center">
+                <button
+                  className="m-2 font-semibold bg-yellow-100 hover:bg-yellow-500 px-4 rounded-lg py-2"
+                  onClick={() => navigate(-1) || navigate("/translations")}
+                >
+                  Go back or Translations
+                </button>
+                <Link
+                  to={`/translations/${id}/update/`}
+                  state={{ input: data.input, output: data.output, id: id }}
+                  className="m-2 font-semibold bg-blue-100 hover:bg-blue-400 px-4 rounded-lg py-2"
+                >
+                  edit
+                </Link>
+                <DeleteDialog id={id} />{" "}
+              </div>
+            </>
+          ) : (
+            <button
+              className="m-2 font-semibold bg-yellow-100 hover:bg-yellow-500 px-4 rounded-lg py-2"
+              onClick={() => navigate(-1) || navigate("/translations")}
             >
-              edit
-            </Link>
-            <DeleteDialog id={id} />
-          </div>
+              Go back or Translations
+            </button>
+          )}
         </>
       )}
     </div>
