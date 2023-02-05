0d22e774a7b3b10762216835e06d0e4e3eedf6bd
mahes1287 <mahes1287@gmail.com>
2023-02-05 15:53:56 +0100

    refactored

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function NotFound() {
-  return <div>Oh... OOOOOOOh... The page is not found..... </div>;
+  return (
+    <div className="container mx-auto p-10 text-2xl">
+      <div data-testid="not-found">Sorry, The page is not found.....</div>
+      <Link to={"/"} className="underline">
+        Home
+      </Link>
+    </div>
+  );
 }
